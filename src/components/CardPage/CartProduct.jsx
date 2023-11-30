e2635e159c37f33f81ecc306d65e5e21b1ea77a8
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2023-11-30 17:20:30 -0500

    update cart csss

--- src/components/CardPage/CartProduct.jsx
+++ src/components/CardPage/CartProduct.jsx
@@ -10,22 +10,25 @@ const CartProduct = ({prod}) => {
     }
 
   return (
-    <section>
-        <header>
-            <img src={prod.product.images[0].url} alt="" />
+    <div className="cart_general"> 
+    <section className="cart_product">
+        <header className="header_cart_product">
+            <img className="img_cart_product" src={prod.product.images[0].url} alt="" />
         </header>
-        <article>
-            <h3>{prod.product.title}</h3>
-            <span>{prod.quantity}</span>
-            <div>
+        <article className="cart_product_info">
+            <h3 className="cart_product_title">{prod.product.title}</h3>
+            <span className="cart_product_q">{prod.quantity}</span>
+            <div className="cart_product_price">
                 <span>Price</span>
                 <span>{prod.product.price}</span>
             </div>
         </article>
-        <button onClick={handleDelete}>
+        <button className="btn_cart_product" onClick={handleDelete}>
         <i className='bx bx-trash'></i>
         </button>
     </section>
+
+    </div>
   )
 }
 
