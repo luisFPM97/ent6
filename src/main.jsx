917ee44b3bf1989bc8b1d5368bbed8e00740d6d3
luisFPM97 <luis.pinzon.m.m@gmail.com>
2026-01-22 13:32:00 -0500

    direct

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {HashRouter, RouterProvider} from 'react-router-dom';
+import {BrowserRouter, RouterProvider} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
     <App />
-    </HashRouter>
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
